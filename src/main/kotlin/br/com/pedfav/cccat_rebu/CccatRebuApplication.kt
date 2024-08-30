def661e68a4eb5d5aa63d9dc2cf8f31a61d30631
pedfav <pedfav@ped-macbook-002.local>
2024-08-29 22:44:30 -0300

    Kick off

---
+++ src/main/kotlin/br/com/pedfav/cccat_rebu/CccatRebuApplication.kt
@@ -0,0 +1,11 @@
+package br.com.pedfav.cccat_rebu
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CccatRebuApplication
+
+fun main(args: Array<String>) {
+	runApplication<CccatRebuApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/pedfav/cccat_rebu/CccatRebuApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.pedfav.cccat_rebu
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CccatRebuApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
